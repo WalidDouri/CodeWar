feeb530a763684f2b35fca974fa70a2c1ce0eacc
Walid Douri <88631028+WalidDouri@users.noreply.github.com>
2022-05-17 23:15:24 -0400

    Day 8

---
+++ 7th Kyu/SumOfOddNumbers.js
@@ -0,0 +1,40 @@
+/*
+
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
+
+*/
+
+//Answer
+
+
+function rowSumOddNumbers(n) {
+  let sum = (n * n) * n
+  return sum
+}
+
+//42 * 42
+//1764*42
+
+
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(rowSumOddNumbers(1), 1);
+    assert.strictEqual(rowSumOddNumbers(42), 74088);
+  });
+});

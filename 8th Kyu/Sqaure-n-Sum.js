bc1d5519462130834f956496ef1b258ed160906e
Walid Douri <88631028+WalidDouri@users.noreply.github.com>
2022-05-17 22:59:35 -0400

    Day 8

---
+++ 8th Kyu/Sqaure-n-Sum.js
@@ -0,0 +1,25 @@
+/*
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+
+*/
+
+//Answer
+
+function squareSum(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i] ** 2;
+  }
+  return sum
+}
+
+describe("Tests", () => {
+  it("test", () => {
+    Test.assertEquals(squareSum([1, 2]), 5)
+    Test.assertEquals(squareSum([0, 3, 4, 5]), 50)
+    Test.assertEquals(squareSum([]), 0)
+  });
+});

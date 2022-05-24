6faecb9690e5fb4e07c520a1bdff6f9808e5ae6a
Walid Douri <88631028+WalidDouri@users.noreply.github.com>
2022-05-23 23:36:15 -0400

    Day 14

---
+++ 8th Kyu/AbbreviateATwoWordName.js
@@ -0,0 +1,40 @@
+/*
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+*/
+
+
+//Answer
+
+function abbrevName(name) {
+  //toUppercase
+  //split -index 0 
+  //  foreach loop??
+  let ish = name.toUpperCase().split(' ');
+  return (ish[0][0] + '.' + ish[1][0]);
+
+}
+
+
+
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(abbrevName("Sam Harris"), "S.H");
+    assert.strictEqual(abbrevName("Patrick Feenan"), "P.F");
+    assert.strictEqual(abbrevName("Evan Cole"), "E.C");
+    assert.strictEqual(abbrevName("P Favuzzi"), "P.F");
+    assert.strictEqual(abbrevName("David Mendieta"), "D.M");
+  });
+});

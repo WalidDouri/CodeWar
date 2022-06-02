6f7a2b921022396896ef27b60a9652433c815e16
Walid Douri <88631028+WalidDouri@users.noreply.github.com>
2022-06-01 20:00:39 -0400

    Day 23

---
+++ 5th Kyu/ExtractTheDomainNameFromAURL.js
@@ -0,0 +1,29 @@
+/*
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+* url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+* url = "https://www.cnet.com"                -> domain name = cnet"
+
+*/
+
+
+//Answer
+function domainName(url) {
+  //your code here
+  return url.replace(/.+\/\/|www.|\..+/g, '')
+}
+
+// https://regexr.com/
+
+
+const { assert } = require("chai");
+
+describe("Sample test", () => {
+  it("Should pass sample tests", () => {
+    assert.equal(domainName("http://google.com"), "google");
+    assert.equal(domainName("http://google.co.jp"), "google");
+    assert.equal(domainName("www.xakep.ru"), "xakep");
+    assert.equal(domainName("https://youtube.com"), "youtube");
+  })
+})

a411f4f3d073dbe6a9ae35d6a8d24c280a3868a8
Walid Douri <88631028+WalidDouri@users.noreply.github.com>
2022-06-04 15:50:51 -0400

    Day 27

---
+++ 7th Kyu/Mubling.js
@@ -0,0 +1,39 @@
+/*
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+*/
+
+
+//Answer
+function accum(s) {
+  // your code
+  let arr = [];
+  let space = s.split("");
+  for (let i = 0; i < space.length; i++) {
+    //index +1 and mutiple it to find the amount it shows.
+    // capitalize the first letter of the string
+    arr.push(space[i].toUpperCase() + Array(i + 1).join(space[i].toLowercase()));
+
+    console.log(space, "-------", arr)
+
+  }
+  return arr
+}
+
+
+
+
+describe("accum", function () {
+  it("Basic tests", function () {
+    Test.assertEquals(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+    Test.assertEquals(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+    Test.assertEquals(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+    Test.assertEquals(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+    Test.assertEquals(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+  })
+})

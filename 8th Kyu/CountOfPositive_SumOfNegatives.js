2e3c66a0d58d9d6dceae48b36708873e923639be
Walid Douri <88631028+WalidDouri@users.noreply.github.com>
2022-08-05 23:14:44 -0400

    Day 89

---
+++ 8th Kyu/CountOfPositive_SumOfNegatives.js
@@ -0,0 +1,48 @@
+/*
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
+
+//Answer
+function countPositivesSumNegatives(input) {
+  // your code here
+  let neg = 0
+  let pos = 0
+  if (input == null || input.length < 1) {
+    return []
+  }
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] < 0) {
+      neg += input[i]
+    } else if (input[i] > 0) {
+      pos++
+    }
+  }
+  return [pos, neg]
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Example tests", () => {
+  it("Testing for fixed test 1", () => {
+    let testData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
+    let actual = countPositivesSumNegatives(testData);
+    let expected = [10, -65];
+    assert.deepEqual(actual, expected);
+  })
+
+  it("Testing for fixed test 2", () => {
+    let testData = [0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14];
+    let actual = countPositivesSumNegatives(testData);
+    let expected = [8, -50];
+    assert.deepEqual(actual, expected);
+  });
+});

a96fcdb3c45f9e53a641b2ff866c1c0798ebf8f5
Walid Douri <88631028+WalidDouri@users.noreply.github.com>
2022-08-11 23:25:32 -0400

    Day 95

---
+++ 8th Kyu/WellOfIdeas_EasyVersion.js
@@ -0,0 +1,32 @@
+/*
+For every good kata idea there seem to be quite a few bad ones!
+
+In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+
+*/
+
+//Answer
+
+function well(x) {
+  let count = 0
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] === 'good') {
+      count++
+    }
+  }
+  return count === 0 ? "Fail!" : count > 2 ? "I smell a series!" : "Publish!";
+
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(well(['bad', 'bad', 'bad']), 'Fail!');
+    assert.strictEqual(well(['good', 'bad', 'bad', 'bad', 'bad']), 'Publish!');
+    assert.strictEqual(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']), 'I smell a series!');
+  });
+});

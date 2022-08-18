06047f9d1008ab0ec683ffe78f3a5c837180a5f6
Walid Douri <88631028+WalidDouri@users.noreply.github.com>
2022-08-17 23:43:07 -0400

    Correct

--- 8th Kyu/ANeedleInAHayStack.js
+++ 8th Kyu/ANeedleInAHayStack.js
@@ -27,6 +27,7 @@ function findNeedle(haystack) {
 
 
 describe("Tests", () => {
+
   it("test", () => {
     var haystack_1 = ['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false];
     var haystack_2 = ['283497238987234', 'a dog', 'a cat', 'some random junk', 'a piece of hay', 'needle', 'something somebody lost a while ago'];
@@ -38,3 +39,4 @@ describe("Tests", () => {
     Test.assertEquals(findNeedle(haystack_3), 'found the needle at position 30')
   });
 });
+

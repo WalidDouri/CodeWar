67a0a56a2aa202b384fbc1b08ec63e094e5aa8e3
Walid Douri <88631028+WalidDouri@users.noreply.github.com>
2022-09-01 22:58:41 -0400

    Day 112

---
+++ 8th Kyu/RegularBallSuperBall.js
@@ -0,0 +1,33 @@
+/*
+Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+ball1.ballType     //=> "regular"
+ball2.ballType     //=> "super"
+*/
+
+//Answers
+
+var Ball = function (ballType) {
+  // your code goes here
+
+  this.ballType = ballType || "regular"
+
+};
+
+
+describe("Tests", () => {
+  it("test", () => {
+    // Create your own tests here. These are some of the methods available:
+    //  Test.expect(boolean, [optional] message) 
+    //  Test.assertEquals(actual, expected, [optional] message)
+    //  Test.assertSimilar(actual, expected, [optional] message)
+    //  Test.assertNotEquals(actual, expected, [optional] message)
+    Test.assertEquals(new Ball().ballType, "regular");
+    Test.assertEquals(new Ball("super").ballType, "super");
+  });
+});

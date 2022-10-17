4555c7e8aa8cf0adf150e1c87e511b3a822506c9
Walid Douri <88631028+WalidDouri@users.noreply.github.com>
2022-10-16 22:28:24 -0400

    Day 152

---
+++ 7th Kyu/SimpleFun176ReverseLetter.js
@@ -0,0 +1,39 @@
+/*
+Given a string str, reverse it and omit all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+A string consists of lowercase latin letters, digits and symbols.
+
+[output] a string
+*/
+
+//Answer
+function reverseLetter(str) {
+  //coding and coding..
+  let apples = str.replace(/[^A-Za-z]+/g, '');
+  let pears = apples.split("").reverse().join('')
+  return pears
+}
+
+
+const Test = require('@codewars/test-compat');
+
+describe("Basic Tests", function () {
+  it("It should works for basic tests.", function () {
+
+    Test.assertEquals(reverseLetter("krishan"), "nahsirk")
+
+    Test.assertEquals(reverseLetter("ultr53o?n"), "nortlu")
+
+    Test.assertEquals(reverseLetter("ab23c"), "cba")
+
+    Test.assertEquals(reverseLetter("krish21an"), "nahsirk")
+
+  })
+})

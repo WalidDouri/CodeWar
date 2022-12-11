4fc733ea32c12441c8b6d7c50fc533dbb5482d39
Walid Douri <88631028+WalidDouri@users.noreply.github.com>
2022-12-10 22:45:11 -0500

    Create Highest Scoring Word.js

---
+++ 6th Kyu/Highest Scoring Word.js
@@ -0,0 +1,48 @@
+/*
+Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid.
+*/
+
+//Answer
+function high(x) {
+
+
+  //split words  map?reduce?indexof? create count
+  let apples = x.split(' ')
+  let cheese = apples.map(x => [...x].reduce((a, b) => a + b.charCodeAt(0) - 96, 0)
+  );
+
+  let pear = cheese.indexOf(Math.max(...cheese))
+
+  return apples[pear]
+
+  console.log(pear)
+
+  console.log(apples, cheese)
+
+
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(high('man i need a taxi up to ubud'), 'taxi');
+    assert.strictEqual(high('what time are we climbing up the volcano'), 'volcano');
+    assert.strictEqual(high('take me to semynak'), 'semynak');
+    assert.strictEqual(high('aa b'), 'aa');
+    assert.strictEqual(high('b aa'), 'b');
+    assert.strictEqual(high('bb d'), 'bb');
+    assert.strictEqual(high('d bb'), 'd');
+    assert.strictEqual(high('aaa b'), 'aaa');
+  })
+});

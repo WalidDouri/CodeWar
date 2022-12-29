80bba51d316c0dd9fc3d14bd38ed1cf6bb8bf8f7
Walid Douri <88631028+WalidDouri@users.noreply.github.com>
2022-12-28 23:37:46 -0500

    Create DoIGetABonus.js

---
+++ 7th Kyu/SortTheGiftCode.js
@@ -0,0 +1,34 @@
+/*
+Happy Holidays fellow Code Warriors!
+Santa's senior gift organizer Elf developed a way to represent up to 26 gifts by assigning a unique alphabetical character to each gift. After each gift was assigned a character, the gift organizer Elf then joined the characters to form the gift ordering code.
+
+Santa asked his organizer to order the characters in alphabetical order, but the Elf fell asleep from consuming too much hot chocolate and candy canes! Can you help him out?
+
+Sort the Gift Code
+Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+
+Examples (Input -- => Output):
+"abcdef"                      -- => "abcdef"
+"pqksuvy"                     -- => "kpqsuvy"
+"zyxwvutsrqponmlkjihgfedcba"  -- => "abcdefghijklmnopqrstuvwxyz"
+*/
+
+
+//Answer
+
+function sortGiftCode(code) {
+
+  console.log(code)
+  let apples = code.split('').sort().join('');
+
+  return apples
+  //   console.log(apples)
+}
+
+describe("Tests", () => {
+  it("test", () => {
+    Test.assertEquals(sortGiftCode('abcdef'), 'abcdef');
+    Test.assertEquals(sortGiftCode('pqksuvy'), 'kpqsuvy');
+    Test.assertEquals(sortGiftCode('zyxwvutsrqponmlkjihgfedcba'), 'abcdefghijklmnopqrstuvwxyz');
+  });
+});

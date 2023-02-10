11a4018f7f06392ddf2073371e84694849bfea94
Walid Douri <88631028+WalidDouri@users.noreply.github.com>
2023-02-09 23:59:54 -0500

    Update Highest Scoring Word.js

--- 6th Kyu/Highest Scoring Word.js
+++ 6th Kyu/Highest Scoring Word.js
@@ -30,8 +30,8 @@ function high(x) {
 
 }
 
-function high(s){
-  let as = s.split(' ').map(s=>[...s].reduce((a,b)=>a+b.charCodeAt(0)-96,0));
+function high(s) {
+  let as = s.split(' ').map(s => [...s].reduce((a, b) => a + b.charCodeAt(0) - 96, 0));
   return s.split(' ')[as.indexOf(Math.max(...as))];
 }
 

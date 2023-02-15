960aab0dedbc7117c6d484e97b38537ab4ce73c1
Walid Douri <88631028+WalidDouri@users.noreply.github.com>
2023-02-14 22:26:36 -0500

    Update DoIGetABonus.js

--- 8th Kyu/DoIGetABonus.js
+++ 8th Kyu/DoIGetABonus.js
@@ -7,7 +7,6 @@ If bonus is true, the salary should be multiplied by 10. If bonus is false, the 
 
 Return the total figure the individual will receive as a string prefixed with "£" (= "\u00A3", JS, Go, Java, Scala, and Julia), "$" (C#, C++, Ruby, Clojure, Elixir, PHP, Python, Haskell, and Lua) or "¥" (Rust).
 
-
 */
 
 
